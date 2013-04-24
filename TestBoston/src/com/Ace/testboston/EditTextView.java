5de5633653186467d38d5e93dfdd50a2afd7eaec
Althona2yPro <Althona2yPro@gmail.com>
2013-04-24 10:15:03 +0200

    test2

--- TestBoston/src/com/Ace/testboston/EditTextView.java
+++ TestBoston/src/com/Ace/testboston/EditTextView.java
@@ -1,8 +1,12 @@
 package com.Ace.testboston;
 
+import java.util.Random;
+
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -17,27 +21,44 @@ public class EditTextView extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.edit_text);
-		TextView tv_edit = (TextView) findViewById(R.id.tvedittext);
+		final TextView tv_edit = (TextView) findViewById(R.id.tvedittext);
 		final EditText et_edit = (EditText) findViewById(R.id.etPassword);
 		Button b_edit = (Button) findViewById(R.id.b_edittext);
 		final ToggleButton tb_edit = (ToggleButton) findViewById(R.id.tbedittext);
-		
-		tb_edit.setOnClickListener(new OnClickListener(){
+
+		tb_edit.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				
-				if (tb_edit.isChecked()){
-					et_edit.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-				}else{
+
+				if (tb_edit.isChecked()) {
+					et_edit.setInputType(InputType.TYPE_CLASS_TEXT
+							| InputType.TYPE_TEXT_VARIATION_PASSWORD);
+				} else {
 					et_edit.setInputType(InputType.TYPE_CLASS_TEXT);
 				}
 			}
-			
-		});
-		
-		
-		
-	}
 
+		});
+		b_edit.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+
+				String check = et_edit.getText().toString();
+
+				if (check.contentEquals("left")) {
+					tv_edit.setTextColor(Color.BLUE);
+					tv_edit.setGravity(Gravity.LEFT);
+					tv_edit.setTextSize(125);
+				} else if (check.contentEquals("love")) {
+					Random crazy = new Random();
+					tv_edit.setText("I Love You");
+					tv_edit.setTextSize(crazy.nextInt(75));
+					tv_edit.setTextColor(Color.argb(crazy.nextInt(265), crazy.nextInt(265), crazy.nextInt(265), crazy.nextInt(265)));
+
+				}
+			}
+		});
+
+	}
 }
